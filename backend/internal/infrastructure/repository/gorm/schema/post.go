f2c6c0e9f3575d958732cd51f8cd0a966430e059
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-11 09:57:53 +0900

    time.timeの修正

--- backend/internal/infrastructure/repository/gorm/schema/post.go
+++ backend/internal/infrastructure/repository/gorm/schema/post.go
@@ -1,12 +1,18 @@
 package schema
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Post struct {
-	ID        int    `gorm:"type:TINYINT AUTO_INCREMENT;not null;primaryKey"`
-	UserID    int    `gorm:"type:TYNYINT;not null"`
-	Title     string `gorm:"type:VARCHAR(100);not null"`
-	Body      string `gorm:"type:TEXT;not null"`
-	CreatedAt string `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt string `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP"`
-	DeletedAt string `gorm:"type:DATETIME NULL;default:NULL"`
-	User      User   `gorm:"foreignKey:UserID"`
+	ID        int            `gorm:"type:TINYINT AUTO_INCREMENT;not null;primaryKey"`
+	UserID    int            `gorm:"type:TYNYINT;not null"`
+	Title     string         `gorm:"type:VARCHAR(100);not null"`
+	Body      string         `gorm:"type:TEXT;not null"`
+	CreatedAt time.Time      `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP"`
+	DeletedAt gorm.DeletedAt `gorm:"type:DATETIME NULL;default:NULL"`
+	User      User           `gorm:"foreignKey:UserID"`
 }
